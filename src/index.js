6590743808b4cd06e2344cc5092e6445eaeefd13
RahinF <s3297352@student.rmit.edu.au>
2022-12-30 12:23:34 +1100

    updated origin

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ connectToDatabase();
 app.use(express.json());
 
 const corsOptions = {
-  origin: [`${process.env.BACKEND_URL}`, "http://localhost:3000"],
+  origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
   credentials: true,
 };
 
